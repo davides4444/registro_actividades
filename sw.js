0a72b1602b193be62dc7d420d252f2afb74333c7
davides4444 <davidmamaniquispe444@gmail.com>
2026-09-02 20:50:02 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'actividades-v1';
+const ASSETS = [
+  './',
+  './index.html'
+];
+
+// Guardar archivos en la memoria al instalar
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// Limpiar cachés antiguas
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
+// Responder desde la memoria si no hay internet
+self.addEventListener('fetch', (e) => {
+  if (e.request.method === 'GET') {
+    e.respondWith(
+      caches.match(e.request).then((cachedResponse) => {
+        return cachedResponse || fetch(e.request);
+      })
+    );
+  }
+});
